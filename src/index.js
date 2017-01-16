c16e91330701240005e095586a64de04ca2c2ae2
Merksue <gabeaboy@gmail.com>
2017-01-16 16:54:40 -0500

    basic start

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let message = 'hellos!!!!!sdsd!!'
+console.log(message);
+import React from 'react'
+import {render} from 'react-dom'
+import App from './components/app'
+
+render(
+  <App />,document.getElementById('app')
+)
