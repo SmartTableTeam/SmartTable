631fdbe98da9c9b408de439c8a59970457f60042
CdrXander <xackhackman@gmail.com>
2017-02-02 21:26:11 -0700

    fixing a filename bug

--- src/components/landing/login/Login.js
+++ src/components/landing/login/Login.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react'
 import {Link} from 'react-router'
 import {hashHistory} from 'react-router'
-import './login.scss'
+import './Login.scss'
 
 export default class Login extends Component {
 
